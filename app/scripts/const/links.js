96effc17336ded7a52b86b2913561013f8c2bdd0
Antelle <dmitryvit@gmail.com>
2015-10-27 23:22:13 +0300

    fix latest release link

--- app/scripts/const/links.js
+++ app/scripts/const/links.js
@@ -2,7 +2,7 @@
 
 var Links = {
     Repo: 'https://github.com/antelle/keeweb',
-    Desktop: 'https://github.com/antelle/keeweb/releases',
+    Desktop: 'https://github.com/antelle/keeweb/releases/latest',
     WebApp: 'https://antelle.github.io/keeweb/',
     License: 'https://github.com/antelle/keeweb/blob/master/MIT-LICENSE.txt'
 };
