06ba534cba79b8e7c6345111bdd4e83b3fc34ed1
Antelle <dmitryvit@gmail.com>
2015-11-14 19:08:31 +0300

    https link

--- app/scripts/const/links.js
+++ app/scripts/const/links.js
@@ -3,7 +3,7 @@
 var Links = {
     Repo: 'https://github.com/antelle/keeweb',
     Desktop: 'https://github.com/antelle/keeweb/releases/latest',
-    WebApp: 'http://antelle.github.io/keeweb/',
+    WebApp: 'https://antelle.github.io/keeweb/',
     License: 'https://github.com/antelle/keeweb/blob/master/MIT-LICENSE.txt',
     UpdateDesktop: 'https://github.com/antelle/keeweb/releases/download/{ver}/UpdateDesktop.zip',
     ReleaseNotes: 'https://github.com/antelle/keeweb/blob/master/release-notes.md#release-notes'
