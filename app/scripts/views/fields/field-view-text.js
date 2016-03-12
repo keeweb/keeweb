898cf5ecb4030dd15b8950b1010b895236385696
Antelle <dmitryvit@gmail.com>
2016-03-12 14:25:25 +0300

    ctrl-key

--- app/scripts/views/fields/field-view-text.js
+++ app/scripts/views/fields/field-view-text.js
@@ -124,7 +124,7 @@ var FieldViewText = FieldView.extend({
         e.stopPropagation();
         var code = e.keyCode || e.which;
         if (code === Keys.DOM_VK_RETURN) {
-            if (!this.model.multiline || (!e.altKey && !e.shiftKey)) {
+            if (!this.model.multiline || (!e.altKey && !e.shiftKey && !e.ctrlKey)) {
                 this.stopListening(Backbone, 'click', this.fieldValueBlur);
                 this.endEdit(e.target.value);
             }
