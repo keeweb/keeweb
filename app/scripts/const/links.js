5878356d59bca9a04f39bb3659c2f4bc4d933693
antelle <dmitryvit@gmail.com>
2016-06-04 10:09:10 +0300

    fix manifest link

--- app/scripts/const/links.js
+++ app/scripts/const/links.js
@@ -10,7 +10,7 @@ var Links = {
     UpdateDesktop: 'https://github.com/antelle/keeweb/releases/download/v{ver}/UpdateDesktop.zip',
     ReleaseNotes: 'https://github.com/antelle/keeweb/blob/master/release-notes.md#release-notes',
     SelfHostedDropbox: 'https://github.com/antelle/keeweb#self-hosting',
-    Manifest: 'https://antelle.github.io/keeweb/manifest.appcache',
+    Manifest: 'https://app.keeweb.info/manifest.appcache',
     AutoType: 'https://github.com/antelle/keeweb/wiki/Auto-Type'
 };
 
