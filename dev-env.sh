8895b5ea6d5b81855583760f3127a5dae71632b3
antelle <dmitryvit@gmail.com>
2017-12-16 12:31:03 +0100

    branches

--- dev-env.sh
+++ dev-env.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 git clone git@github.com:keeweb/favicon-proxy.git
 git clone git@github.com:keeweb/kdbxweb.git
-git clone git@github.com:keeweb/keeweb.git
+git clone git@github.com:keeweb/keeweb.git -b develop
 git clone git@github.com:keeweb/beta.keeweb.info.git keeweb-beta
-git clone git@github.com:keeweb/keeweb-site.git
+git clone git@github.com:keeweb/keeweb-site.git -b gh-pages
 git clone git@github.com:keeweb/keeweb-plugins.git
 git clone git@github.com:keeweb/keeweb.git keeweb-dist -b gh-pages --single-branch
 mkdir keys
