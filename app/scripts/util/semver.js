925b7463e99bb728520a1b93e7c8881294d9cb1b
antelle <dmitryvit@gmail.com>
2017-12-16 13:30:07 +0100

    fixed version comparison

--- app/scripts/util/semver.js
+++ app/scripts/util/semver.js
@@ -1,7 +1,7 @@
 const SemVer = {
     compareVersions(left, right) {
-        left = left.split('.');
-        right = right.split('.');
+        left = left.replace(/-.*$/, '').split('.');
+        right = right.replace(/-.*$/, '').split('.');
         for (let num = 0; num < left.length; num++) {
             const partLeft = left[num] | 0;
             const partRight = right[num] | 0;
