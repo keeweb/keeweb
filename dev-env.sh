e709c047a6e3e83ca84953d023164c9903c0a52c
antelle <dmitryvit@gmail.com>
2017-12-16 12:35:31 +0100

    dev-env script paths

--- dev-env.sh
+++ dev-env.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-git clone git@github.com:keeweb/favicon-proxy.git
-git clone git@github.com:keeweb/kdbxweb.git
-git clone git@github.com:keeweb/keeweb.git -b develop
-git clone git@github.com:keeweb/beta.keeweb.info.git keeweb-beta
-git clone git@github.com:keeweb/keeweb-site.git -b gh-pages
-git clone git@github.com:keeweb/keeweb-plugins.git
-git clone git@github.com:keeweb/keeweb.git keeweb-dist -b gh-pages --single-branch
-mkdir keys
-echo kdbxweb/ > .eslintignore
+echo Cloning KeeWeb into $PWD/keeweb...
+mkdir keeweb
+git clone git@github.com:keeweb/favicon-proxy.git keeweb/favicon-proxy
+git clone git@github.com:keeweb/kdbxweb.git keeweb/kdbxweb
+git clone git@github.com:keeweb/keeweb.git -b develop keeweb/keeweb
+git clone git@github.com:keeweb/beta.keeweb.info.git keeweb/keeweb-beta
+git clone git@github.com:keeweb/keeweb-site.git -b gh-pages keeweb/keeweb-site
+git clone git@github.com:keeweb/keeweb-plugins.git keeweb/keeweb-plugins
+git clone git@github.com:keeweb/keeweb.git -b gh-pages --single-branch keeweb/keeweb-dist
+mkdir keeweb/keys
+echo kdbxweb/ > keeweb/.eslintignore
+echo Done! KeeWeb is cloned into $PWD/keeweb
