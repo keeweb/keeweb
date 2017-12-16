3ca10b1dfe5571ca3b99404435b13e106308501e
antelle <dmitryvit@gmail.com>
2017-12-16 12:28:22 +0100

    dev-env script: fixed beta path

--- dev-env.sh
+++ dev-env.sh
@@ -2,7 +2,7 @@
 git clone git@github.com:keeweb/favicon-proxy.git
 git clone git@github.com:keeweb/kdbxweb.git
 git clone git@github.com:keeweb/keeweb.git
-git clone git@github.com:keeweb/keeweb-beta.git
+git clone git@github.com:keeweb/beta.keeweb.info.git keeweb-beta
 git clone git@github.com:keeweb/keeweb-site.git
 git clone git@github.com:keeweb/keeweb-plugins.git
 git clone git@github.com:keeweb/keeweb.git keeweb-dist -b gh-pages --single-branch
