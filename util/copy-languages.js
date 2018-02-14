581f4e97587d2d2f22079ae448b4a683c34564a1
Anderson Silva <ams.bra@gmail.com>
2018-02-14 16:11:14 -0200

    Add support to pt-BR
    Add native support to Portuguese Brazilian

--- util/copy-languages.js
+++ util/copy-languages.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const langs = ['de-DE', 'fr-FR'];
+const langs = ['de-DE', 'fr-FR', 'pt-BR'];
 
 for (const lang of langs) {
     fs.writeFileSync(`app/scripts/locales/${lang}.json`, fs.readFileSync(`../keeweb-plugins/docs/translations/${lang}/${lang}.json`));
