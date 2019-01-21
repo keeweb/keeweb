28c9faf48f79e296ead881696dc0f68f793c909e
antelle <dmitryvit@gmail.com>
2019-01-21 20:52:40 +0100

    focusdetector static

--- app/scripts/comp/focus-detector.js
+++ app/scripts/comp/focus-detector.js
@@ -1,22 +1,24 @@
 const FeatureDetector = require('../util/feature-detector');
 const Launcher = require('../comp/launcher');
 
-const FocusDetector = function () {
-    this.isFocused = true;
-    this.detectsFocusWithEvents = !FeatureDetector.isDesktop && !FeatureDetector.isMobile;
-    if (this.detectsFocusWithEvents) {
-        window.onblur = () => { this.isFocused = false; };
-        window.onfocus = () => { this.isFocused = true; };
-    }
-};
+const FocusDetector = {
+    init() {
+        this.isFocused = true;
+        this.detectsFocusWithEvents = !FeatureDetector.isDesktop && !FeatureDetector.isMobile;
+        if (this.detectsFocusWithEvents) {
+            window.onblur = () => { this.isFocused = false; };
+            window.onfocus = () => { this.isFocused = true; };
+        }
+    },
 
-FocusDetector.prototype.hasFocus = function () {
-    if (this.detectsFocusWithEvents) {
-        return this.isFocused;
-    } else if (Launcher) {
-        return Launcher.isAppFocused();
-    } else {
-        return true;
+    hasFocus() {
+        if (this.detectsFocusWithEvents) {
+            return this.isFocused;
+        } else if (Launcher) {
+            return Launcher.isAppFocused();
+        } else {
+            return true;
+        }
     }
 };
 
