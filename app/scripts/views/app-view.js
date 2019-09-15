0028f2293f69003e190efcbff4c20e2e411ff9fd
antelle <dmitryvit@gmail.com>
2019-09-15 07:47:45 +0200

    checking if devtools are supported before adding a shortcut

--- app/scripts/views/app-view.js
+++ app/scripts/views/app-view.js
@@ -97,12 +97,14 @@ const AppView = Backbone.View.extend({
 
         KeyHandler.onKey(Keys.DOM_VK_ESCAPE, this.escPressed, this);
         KeyHandler.onKey(Keys.DOM_VK_BACK_SPACE, this.backspacePressed, this);
-        KeyHandler.onKey(
-            Keys.DOM_VK_I,
-            this.openDevTools,
-            this,
-            KeyHandler.SHORTCUT_ACTION + KeyHandler.SHORTCUT_OPT
-        );
+        if (Launcher && Launcher.devTools) {
+            KeyHandler.onKey(
+                Keys.DOM_VK_I,
+                this.openDevTools,
+                this,
+                KeyHandler.SHORTCUT_ACTION + KeyHandler.SHORTCUT_OPT
+            );
+        }
 
         setInterval(this.syncAllByTimer.bind(this), Timeouts.AutoSync);
 
