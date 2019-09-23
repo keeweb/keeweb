6aca16c336e57ac52865134d81cccd910da7ef3c
antelle <dmitryvit@gmail.com>
2019-09-23 20:36:54 +0200

    prettified service worker

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,16 +1,16 @@
 const VERSION = '0.0.0';
 
-self.addEventListener('install', event => {
+self.addEventListener('install', event =>
     event.waitUntil(
-        caches.open('v1').then(cache => {
-            return fetch('.?v=' + VERSION).then(response => {
+        caches.open('v1').then(cache =>
+            fetch('.?v=' + VERSION).then(response => {
                 if (response.ok) {
-                    cache.put('.', response);
+                    return cache.put('.', response);
                 }
-            });
-        })
-    );
-});
+            })
+        )
+    )
+);
 
 self.addEventListener('fetch', event => {
     event.respondWith(
