7b4953a0490dd4ce6ce500f221908f16e136d6c4
Thoralf Rickert-Wendt <thoralf@m84.de>
2019-10-11 17:33:50 +0200

    #1298 add variable to define a custom configuration within docker

--- package/docker/entrypoint.sh
+++ package/docker/entrypoint.sh
@@ -31,6 +31,11 @@ then
   -days 3650 -nodes -sha256
 fi
 
+if [ ${KEEWEB_CONFIG_URL} ]
+then
+  sed -i /\(no content\)/${KEEWEB_CONFIG_URL}/g keeweb/index.html
+fi
+
 # exec CMD
 echo ">> exec docker CMD"
 echo "$@"
