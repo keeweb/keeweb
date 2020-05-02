d36588d14823e5145df0ebefe9faff16591d69c1
antelle <dmitryvit@gmail.com>
2020-05-02 19:54:47 +0200

    ignored the whole file in prettier

--- grunt.entrypoints.js
+++ grunt.entrypoints.js
@@ -1,28 +1,25 @@
+// prettier-ignore
+
 module.exports = function(grunt) {
-    // prettier-ignore
     grunt.registerTask('default', 'Default: build web app', [
         'build-web-app'
     ]);
 
-    // prettier-ignore
     grunt.registerTask('dev', 'Build project and start web server and watcher', [
         'build-web-app',
         'devsrv'
     ]);
 
-    // prettier-ignore
     grunt.registerTask('devsrv', 'Start web server and watcher', [
         'webpack-dev-server'
     ]);
 
-    // prettier-ignore
     grunt.registerTask('desktop', 'Build web and desktop apps for all platforms', [
         'test',
         'default',
         'build-desktop'
     ]);
 
-    // prettier-ignore
     grunt.registerTask('desktop-linux', 'Build desktop apps on linux', [
         'gitinfo',
         'clean:desktop',
@@ -33,7 +30,6 @@ module.exports = function(grunt) {
         'build-desktop-dist-linux'
     ]);
 
-    // prettier-ignore
     grunt.registerTask('desktop-darwin', 'Build desktop apps on macos', [
         'gitinfo',
         'clean:desktop',
@@ -42,7 +38,6 @@ module.exports = function(grunt) {
         'build-desktop-dist-darwin'
     ]);
 
-    // prettier-ignore
     grunt.registerTask('desktop-win32', 'Build desktop apps on windows', [
         'gitinfo',
         'clean:desktop',
@@ -52,18 +47,15 @@ module.exports = function(grunt) {
         'build-desktop-dist-win32'
     ]);
 
-    // prettier-ignore
     grunt.registerTask('finish-release', 'Complete the release started with desktop-*', [
         'sign-dist'
     ]);
 
-    // prettier-ignore
     grunt.registerTask('cordova', 'Build cordova app', [
         'default',
         'build-cordova'
     ]);
 
-    // prettier-ignore
     grunt.registerTask('test', 'Build and run tests', [
         'build-test',
         'run-test'
