b5fddeb6271b155bf1597b83553597e500d8deff
antelle <dmitryvit@gmail.com>
2020-05-24 18:42:14 +0200

    added a missing check

--- app/scripts/comp/app/yubikey.js
+++ app/scripts/comp/app/yubikey.js
@@ -188,11 +188,17 @@ const YubiKey = {
     },
 
     getOtp(serial, entry, callback) {
+        if (this.process) {
+            return callback('Already in progress');
+        }
+        this.aborted = false;
+
         return Launcher.spawn({
             cmd: 'ykman',
             args: ['-d', serial, 'oath', 'code', '--single', entry],
             noStdOutLogging: true,
             complete: (err, stdout) => {
+                this.process = null;
                 if (err) {
                     return callback(err);
                 }
