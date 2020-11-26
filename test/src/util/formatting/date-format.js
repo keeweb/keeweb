4b65792585f1fc5a089c28201c7c9a6cd253334b
Giuseppe <baittiner.giuseppe.dev@gmail.com>
2020-11-26 20:48:43 +0100

    Tests now are UTC based to avoid locale conflicts

--- test/src/util/formatting/date-format.js
+++ test/src/util/formatting/date-format.js
@@ -2,14 +2,14 @@ import { expect } from 'chai';
 import { DateFormat } from 'util/formatting/date-format';
 
 describe('DateFormat', () => {
-    const dt = new Date(2020, 0, 2, 3, 4, 5, 6);
+    const dt = new Date.UTC(2020, 0, 2, 3, 4, 5, 6);
 
     it('should format date', () => {
-        expect(DateFormat.dStr(dt)).to.eql('2 Jan 2020');
+        expect(DateFormat.dStr(dt)).to.eql('Jan 2, 2020');
     });
 
     it('should format date and time', () => {
-        expect(DateFormat.dtStr(dt)).to.eql('2 Jan 2020 03:04:05');
+        expect(DateFormat.dtStr(dt)).to.eql('Jan 2, 2020, 03:04:05 AM');
     });
 
     it('should format date and time in sortable format', () => {
