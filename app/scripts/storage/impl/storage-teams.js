062ddb3f729c3f4a876451940f18cb195a68bdff
antelle <dmitryvit@gmail.com>
2021-07-13 19:04:27 +0200

    added MS Teams apps

--- app/scripts/storage/impl/storage-teams.js
+++ app/scripts/storage/impl/storage-teams.js
@@ -1,5 +1,5 @@
 import { StorageBase } from 'storage/storage-base';
-import { TeamsApps } from 'const/cloud-storage-apps';
+import { MsTeamsApps } from 'const/cloud-storage-apps';
 import { Features } from 'util/features';
 
 // https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow
