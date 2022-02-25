afb55a4ad7b623f1a371159f151f6e842d3591ad
Ragnar Stølsmark <ragnar@identitystream.com>
2022-02-25 14:12:19 +0100

    Removed prettierignore since we will use npm to distribute files to servicemanager repo

--- app/content/ie.js
+++ app/content/ie.js
@@ -1,10 +1,10 @@
 if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
-	// eslint-disable-next-line prefer-arrow-callback
-	document.addEventListener("DOMContentLoaded", function () {
-		document.body.style.maxWidth = "600px";
-		document.body.style.margin = "40px auto";
-		document.body.style.position = "static";
-		document.body.innerHTML +=
-			"<p>IDS password bank does not support Internet Explorer. Please use Google Chrome instead.</p>";
-	});
+    // eslint-disable-next-line prefer-arrow-callback
+    document.addEventListener('DOMContentLoaded', function () {
+        document.body.style.maxWidth = '600px';
+        document.body.style.margin = '40px auto';
+        document.body.style.position = 'static';
+        document.body.innerHTML +=
+            '<p>IDS password bank does not support Internet Explorer. Please use Google Chrome instead.</p>';
+    });
 }
