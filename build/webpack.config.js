08b621907a5d4d440c497b48c7a98b90d7bcb3aa
Kevin McCormack <kevin@mccormack.tech>
2024-04-06 15:53:29 -0400

    ESLint fixes

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -6,7 +6,7 @@ const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const rootDir = path.join(__dirname, '..');
