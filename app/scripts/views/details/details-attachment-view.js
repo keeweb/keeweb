4ed4472788bf155114df1327534177e233323e22
Aetherinox <aetherinox@proton.me>
2024-04-25 19:31:56 -0700

    refactor: misc minor edits

--- app/scripts/views/details/details-attachment-view.js
+++ app/scripts/views/details/details-attachment-view.js
@@ -16,23 +16,33 @@ class DetailsAttachmentView extends View {
         super.render({
             isMobile: Features.isMobile
         });
+
         const shortcut = this.$el.find('.details__attachment-preview-download-text-shortcut');
         shortcut.text(Shortcuts.actionShortcutSymbol());
         const blob = new Blob([this.model.getBinary()], { type: this.model.mimeType });
         const dataEl = this.$el.find('.details__attachment-preview-data');
 
         switch ((this.model.mimeType || this.model.ext || '').split('/')[0]) {
+            /*
+                File Type > Text
+            */
+
             case 'text': {
                 const reader = new FileReader();
                 reader.addEventListener('loadend', () => {
                     $('<pre/>').text(reader.result).appendTo(dataEl);
                     complete();
                 });
+
                 reader.readAsText(blob);
                 return;
             }
+
+            /*
+                File Type > Markdown
+            */
+
             case 'md': {
-                console.log('markdown');
                 const reader = new FileReader();
                 reader.addEventListener('loadend', () => {
                     const converted = MdToHtml.convert(reader.result);
@@ -46,14 +56,21 @@ class DetailsAttachmentView extends View {
                     $('<div>').html(text).appendTo(dataEl);
                     complete();
                 });
+
                 reader.readAsText(blob);
                 return;
             }
+
+            /*
+                File Type > Image
+            */
+
             case 'image':
                 $('<img/>').attr('src', URL.createObjectURL(blob)).appendTo(dataEl);
                 complete();
                 return;
         }
+
         this.$el.addClass('details__attachment-preview--empty');
         this.$el.find('.details__attachment-preview-icon').addClass('fa-' + this.model.icon);
         complete();
