ee4aa243c3a2ebba04c94b149379038c510929b9
Aetherinox <aetherinox@proton.me>
2024-12-11 08:13:36 -0700

    feat(dev): add log level dev for internal reporting
    adds an additional log level when keeweb is in development mode, disabled in production

--- app/scripts/util/logger.js
+++ app/scripts/util/logger.js
@@ -1,20 +1,43 @@
+import { RuntimeInfo } from 'const/runtime-info';
+
+/*
+    Log Levels > Define
+*/
+
 const Level = {
     Off: 0,
     Error: 1,
     Warn: 2,
     Info: 3,
     Debug: 4,
-    All: 5
+    All: 5,
+    Dev: 6
 };
 
-const MaxLogsToSave = 100;
+function getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+}
 
+/*
+    Max number of logs to save to file before overwrite begins
+*/
+
+const MaxLogsToSave = 100;
 const lastLogs = [];
 
 const Logger = function (name, id, level = Level.All) {
     this.prefix = name ? name + (id ? ':' + id : '') : 'default';
     this.level = level;
 };
+
+/*
+    Timestamp (performance in milliseconds)
+    track performance time with
+
+        const ts = logger.ts();
+        <ACTIONS TO COMPLETE>
+        logger.debug('Complete', logger.ts(ts));
+*/
 
 Logger.prototype.ts = function (ts) {
     if (ts) {
@@ -24,49 +47,105 @@ Logger.prototype.ts = function (ts) {
     }
 };
 
+/*
+    Get log type prefix
+*/
+
 Logger.prototype.getPrefix = function () {
-    return new Date().toISOString() + ' [' + this.prefix + '] ';
+    return (
+        new Date().toISOString() +
+        ' [' +
+        this.prefix +
+        '] (' +
+        getKeyByValue(Level, this.level) +
+        ') '
+    );
 };
+
+/*
+    Log Level > Dev
+
+    specialized log level. really shouldn't be needed by users. helps output more
+    technical logs. log level needs manually set since Level.All (5) is technically the highest.
+*/
+
+Logger.prototype.dev = function (...args) {
+    this.level = Level.Dev;
+    args[0] = this.getPrefix() + args[0];
+    if (RuntimeInfo.devMode) {
+        Logger.saveLast('dev', args);
+        console.log(...args);
+    }
+    this.level = Level.All;
+};
+
+/*
+    Log Level > Debug
+*/
 
 Logger.prototype.debug = function (...args) {
     args[0] = this.getPrefix() + args[0];
     if (this.level >= Level.Debug) {
         Logger.saveLast('debug', args);
-        console.log(...args); // eslint-disable-line no-console
+        console.log(...args);
     }
 };
+
+/*
+    Log Level > Info
+*/
 
 Logger.prototype.info = function (...args) {
     args[0] = this.getPrefix() + args[0];
     if (this.level >= Level.Info) {
         Logger.saveLast('info', args);
-        console.info(...args); // eslint-disable-line no-console
+        console.info(...args);
     }
 };
+
+/*
+    Log Level > Warn
+*/
 
 Logger.prototype.warn = function (...args) {
     args[0] = this.getPrefix() + args[0];
     if (this.level >= Level.Warn) {
         Logger.saveLast('warn', args);
-        console.warn(...args); // eslint-disable-line no-console
+        console.warn(...args);
     }
 };
+
+/*
+    Log Level > Error
+*/
 
 Logger.prototype.error = function (...args) {
     args[0] = this.getPrefix() + args[0];
     if (this.level >= Level.Error) {
         Logger.saveLast('error', args);
-        console.error(...args); // eslint-disable-line no-console
+        console.error(...args);
     }
 };
+
+/*
+    Log Level > Action > Set
+*/
 
 Logger.prototype.setLevel = function (level) {
     this.level = level;
 };
 
+/*
+    Log Level > Action > Get
+*/
+
 Logger.prototype.getLevel = function () {
     return this.level;
 };
+
+/*
+    Log Level > Action > Save Last
+*/
 
 Logger.saveLast = function (level, args) {
     lastLogs.push({ level, args: Array.prototype.slice.call(args) });
@@ -74,6 +153,10 @@ Logger.saveLast = function (level, args) {
         lastLogs.shift();
     }
 };
+
+/*
+    Log Level > Action > Get Last
+*/
 
 Logger.getLast = function () {
     return lastLogs;
