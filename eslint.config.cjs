91cf1fed191d5da717223b4193b83dcf5bf5de35
Aetherinox <aetherinox@proton.me>
2024-12-14 20:55:57 -0700

    chore(linter): convert from eslint .rc v8 to v9 flat config
    migrate over to the new eslint v9 flat config setup.

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,3 +1,16 @@
+/*
+    Eslint 9 Flat Config
+
+    old eslint < 8 .rc files are no longer supported! do not place .eslintrc files in subfolders.
+    eslint developers are currently working on an experimental feature to allow for sub-folder
+    override rules
+    @ref        https://github.com/eslint/eslint/discussions/18574#discussioncomment-9729092
+                https://eslint.org/docs/latest/use/configure/configuration-files#experimental-configuration-file-resolution
+    
+    eslint config migration docs
+    @ref        https://eslint.org/docs/latest/use/configure/migration-guide
+*/
+
 const js = require('@eslint/js');
 const globals = require('globals');
 
@@ -167,7 +180,51 @@ module.exports = [
             'import/no-default-export': 'error',
             'babel/no-unused-expressions': 'off',
             'node/no-callback-literal': 0,
+
+            /*
+                @plugin         eslint-plugin-n
+            */
+
             'n/no-callback-literal': 0,
+            'n/no-deprecated-api': 'error',
+            'n/no-exports-assign': 'error',
+            'n/no-extraneous-import': 'error',
+            'n/no-extraneous-require': [
+                'error',
+                {
+                    'allowModules': ['electron', 'electron-notarize'],
+                    'resolvePaths': [],
+                    'tryExtensions': []
+                }
+            ],
+            'n/no-hide-core-modules': 'off',
+            'n/no-missing-import': 'off',
+            'n/no-missing-require': 'off',
+            'n/no-mixed-requires': 'error',
+            'n/no-new-require': 'error',
+            'n/no-path-concat': 'error',
+            'n/no-process-env': 'off',
+            'n/no-process-exit': 'off',
+            'n/no-restricted-import': 'error',
+            'n/no-restricted-require': 'error',
+            'n/no-sync': 'off',
+            'n/no-unpublished-bin': 'error',
+            'n/no-unpublished-import': 'error',
+            'n/no-unpublished-require': 'error',
+            'n/no-unsupported-features/es-builtins': 'error',
+            'n/no-unsupported-features/es-syntax': 'error',
+            'n/no-unsupported-features/node-builtins': 'off',
+            'n/prefer-global/buffer': 'error',
+            'n/prefer-global/console': 'error',
+            'n/prefer-global/process': 'error',
+            'n/prefer-global/text-decoder': 'error',
+            'n/prefer-global/text-encoder': 'error',
+            'n/prefer-global/url': 'error',
+            'n/prefer-global/url-search-params': 'error',
+            'n/prefer-node-protocol': 'error',
+            'n/prefer-promises/dns': 'off',
+            'n/prefer-promises/fs': 'off',
+            'n/process-exit-as-throw': 'error',
 
             'prettier/prettier': [
                 'error',
